aebb2722b8455fa5354063e93599724062991d7a
vikasreddym <32944383+vikasreddym@users.noreply.github.com>
2017-11-21 21:29:36 -0600

    word class

---
+++ WordCount/src/WordCount.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class WordCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = new String("Hello! How are you?");
+		StringTokenizer st = new StringTokenizer(s);
+		System.out.println(st.countTokens());
+		
+	}
+
+}
